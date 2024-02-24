41bc5b9b76cc5091351191556c8037559b18ef60
manish-107 <creativecrew2676@gmail.com>
2024-02-24 15:55:03 +0530

    some fix

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import { signup, signin, updateUser } from "../controllers/usersContriller.js";
 import express from "express";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
 router.route('/user/signup').post(signup);
 router.route('/user/signin').post(signin);
-router.route('/user/').put(updateUser);
+router.route('/user/').put(authMiddleware, updateUser);
 
 export default router
