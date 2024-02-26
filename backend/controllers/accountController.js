52b9c7b4984c4e983d979478ac95695b2c1e2ccb
manish-107 <creativecrew2676@gmail.com>
2024-02-26 21:10:46 +0530

    get balance

--- backend/controllers/accountController.js
+++ backend/controllers/accountController.js
@@ -1,7 +1,16 @@
 import { asyncHandler } from "../middlewares/asyncHandler";
+import { accountModel } from "../models/accountModel";
 
 const getBalance = asyncHandler(async (req, res) => {
+    const userId = await req.userId;
 
+    const account = await accountModel.findOne({
+        userId
+    })
+
+    res.json({
+        balance: account.balance
+    })
 })
 
 
