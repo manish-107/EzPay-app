e5cf57044007d1049afe098f14306c9d7870c3a4
manish-107 <creativecrew2676@gmail.com>
2024-02-27 20:11:09 +0530

    added transfer controller

--- backend/controllers/accountController.js
+++ backend/controllers/accountController.js
@@ -1,5 +1,7 @@
+import mongoose from "mongoose";
 import { asyncHandler } from "../middlewares/asyncHandler";
 import { accountModel } from "../models/accountModel";
+import { userModel } from "../models/userModel";
 
 const getBalance = asyncHandler(async (req, res) => {
     const userId = await req.userId;
@@ -13,6 +15,44 @@ const getBalance = asyncHandler(async (req, res) => {
     })
 })
 
+const transferAmt = asyncHandler(async (req, res) => {
+
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    const { amount, to } = req.body;
+
+    const account = await userModel.findOne({
+        userId: req.userId
+    }).session(session);
+
+    if (account.balance < amount) {
+        await session.abortTransaction();
+        return res.status(400).json({
+            message: "Insufficient balance"
+        })
+    }
+
+    const toAccount = await userModel.findOne({
+        userId: to
+    }).session(session);
+
+    if (!toAccount) {
+        session.abortTransaction();
+        res.status(400).json({
+            message: "User not found"
+        })
+    }
+
+    await userModel.updateOne({ userId: req.userId }, { $inc: { balance: -amount } }).session(session);
+    await userModel.updateOne({ userId: to }, { $inc: { balance: amount } }).session(session);
+
+    await session.commitTransaction();
+    res.json({
+        message: "transfer completed"
+    });
 
 
-export { getBalance }
+})
+
+
+export { getBalance, transferAmt }
--- backend/routes/accountRoutes.js
+++ backend/routes/accountRoutes.js
@@ -1,8 +1,9 @@
+import { getBalance, transferAmt } from "../controllers/accountController";
 import { authMiddleware } from "../middlewares/authMiddleware";
 
 const accRouter = express.router();
 
-accRouter.route('/transfer').post(authMiddleware);
+accRouter.route('/transfer').post(authMiddleware, transferAmt);
 accRouter.route('/balance').get(authMiddleware, getBalance);
 
 export default accRouter
